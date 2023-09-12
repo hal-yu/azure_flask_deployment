2e89e5b4b95b275927ca84442e80df81070c0271
yu-hal <haley.yu@stonybrook.edu>
2023-09-12 02:27:33 +0000

    edit and add files

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template
-import pandas as pd
+import random
+from faker import Faker
+
+fake = Faker()
+
+# small change
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def mainpage():
     return render_template('base.html')
 
 @app.route('/about')
-def about():
+def aboutpage():
     return render_template('about.html')
 
-df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA_504_2023/main/WK1/data/113243405_StonyBrookSouthamptonHospital_StandardCharges.csv')
-@app.route('/data')
-def data(data=df):
-    data = data.sample(15)
-    return render_template('data.html', data=data)
+@app.route('/random')
+def randomnumber():
+    number_var = random.randint(1, 100000)
+    fake_address = fake.address()
+    return render_template('random.html', single_number = number_var, single_address = fake_address)
 
 if __name__ == '__main__':
     app.run(
