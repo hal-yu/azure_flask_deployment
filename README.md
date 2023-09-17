f7f08f43412e174b51052b1fe7d2171f3d181ef6
yu-hal <haley.yu@stonybrook.edu>
2023-09-17 22:50:39 +0000

    created diabetes.html and step-by-step guide on readme file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # azure_flask_deployment
 Get hands-on experience setting up a Flask application, integrate it with data processing via Pandas, and deploy the app on Azure App Service. You'll also practice documenting your process and using GitHub for project management.
+
+# Step-By-Step Guide Creating a New Data
+1. Copy codes from Wk 2 folder from Professor Williams
+2. Go to Haley's Week 2 Homework for HHA 507 and copy the raw data from diabetes.csv
+3. Create a new file called diabetes.html
+4. Go to app.py and create a new route to diabetes
+5. Go to base.html and add in the new button to add diabetes to the list
+6. Edit diabetes.html by copying information from data.html and change the header of the cells to fit the diabetes dataset
+
+# Deploy to Azure
+1. On google shell, copy the link  to get the CLI, test that it is installed and login with it
+2. Create new web app service using "az webapp up --resource-group <name> --name <app-name> --runtime <PYTHON: 3.9> --sku <B1>"
+
+### Azure Domain:
+haley-504-flask-azuredep.azurewebsites.net
--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template
-import random
-from faker import Faker
-
-fake = Faker()
-
-# small change
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
-def mainpage():
+def index():
     return render_template('base.html')
 
 @app.route('/about')
-def aboutpage():
+def about():
     return render_template('about.html')
 
-@app.route('/random')
-def randomnumber():
-    number_var = random.randint(1, 100000)
-    fake_address = fake.address()
-    return render_template('random.html', single_number = number_var, single_address = fake_address)
+df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA_504_2023/main/WK1/data/113243405_StonyBrookSouthamptonHospital_StandardCharges.csv')
+@app.route('/data')
+def data():
+    sample_data = df.sample(15)
+    return render_template('data.html', data=sample_data)
+
+df1 = pd.read_csv('https://raw.githubusercontent.com/hal-yu/datasci_2_manipulation/main/dataset/diabetes.csv')
+@app.route('/diabetes')
+def diabetes():
+    sample_data1 = df1.sample(20)
+    return render_template('diabetes.html', diabetes=sample_data1)
 
 if __name__ == '__main__':
     app.run(
